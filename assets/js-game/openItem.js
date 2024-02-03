fef1c428bb9033537711c5074077f7aa390e408e
sebunz <101024918+sebunz@users.noreply.github.com>
2024-02-02 17:32:35 -0800

    Add files via upload

---
+++ assets/js-game/openItem.js
@@ -0,0 +1,43 @@
+function openBlockpost() {
+    var win = window.open()
+    var url = "https://ikatchelo.github.io/blockpost/"
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.border = "none"
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+}
+
+function openBitlife() {
+    var win = window.open()
+    var url = "https://surfdoge.pro/service/hvtrs8%2F-dgrrmcn%2Ccmdgbgre.raee-eahm%2FBmcil%2F%60ivlkfg%2F"
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.border = "none"
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+}
+
+function openShootergame() {
+    var win = window.open()
+    var url = "https://now.gg/apps/justplay/51614/1v1-lol.html"
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.border = "none"
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+}
+
+function openRoblox() {
+    var win = window.open()
+    var url = "https://www.gamenora.com/game/roblox/"
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.border = "none"
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+}
