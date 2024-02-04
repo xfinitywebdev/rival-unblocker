97db250e315f4311e56fb076609c9c315fe15d78
broplswork <143388875+broplswork@users.noreply.github.com>
2024-02-04 04:44:57 +0000

    Update index.js

--- assets/uv/index.js
+++ assets/uv/index.js
@@ -1,19 +1,9 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
-importScripts('uv.bundle.js');
-importScripts('uv.config.js'); // Import the UV config
-importScripts(__uv$config.sw || 'uv.sw.js'); // Use UV config to determine the path to the service worker script
+import { sw } from './uv.config.js'; // Import the UVServiceWorker instance
 
-const sw = new UVServiceWorker(__uv$config); // Pass UV config to UVServiceWorker constructor
-
+// Add fetch event listener
 self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
 
-// Add a message event listener to receive messages from the client
+// Add message event listener for search functionality
 self.addEventListener('message', (event) => {
   if (event.data.msg === 'search') {
     // Extract the search query from the message data
