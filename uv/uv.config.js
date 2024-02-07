85c744946f48a63eb8f116d282a1d6cef03ed817
Potato <105754685+dragon731012@users.noreply.github.com>
2024-02-07 18:48:18 -0500

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
