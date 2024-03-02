92069cecf74e7e55106e493b5ec51b626906b6e8
sebunz <101024918+sebunz@users.noreply.github.com>
2024-03-02 13:36:37 -0800

    Update uv.sw.js

--- uv/uv.sw.js
+++ uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('uv.bundle.js');
+importScripts('uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
