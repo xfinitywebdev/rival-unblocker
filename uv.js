23faa437a501739784859d456428a17b329e2676
sebunz <101024918+sebunz@users.noreply.github.com>
2024-03-02 13:35:18 -0800

    Update uv.js

--- uv.js
+++ uv.js
@@ -1,6 +1,6 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
+importScripts('uv/uv.bundle.js');
+importScripts('uv/uv.config.js');
+importScripts('uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
